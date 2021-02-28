f0638dca321581d0d14dc5c8ba61694ba9898fe6
ericcot2@gamil.com <ericcot2@gamil.com>
2021-02-27 21:20:06 -0600

    add

--- src/main/java/frc/robot/subsystems/ChassisSubsystem.java
+++ src/main/java/frc/robot/subsystems/ChassisSubsystem.java
@@ -83,7 +83,7 @@ public class ChassisSubsystem extends Subsystem{
             chassisMotorR2.set(0.0);
         }
         SmartDashboard.putNumber("Encoder position", encoderL.getPosition());
-        System.out.println(encoderL.getPosition());
+        System.out.println(encoderR.getPosition());
     }
 
     public void moveBackward(double rotation, double speed){
