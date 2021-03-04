e6d3ac22f37125b699a60c6affe88bbdf506ada5
ericcot2@gamil.com <ericcot2@gamil.com>
2021-03-03 21:10:31 -0600

    add

--- src/main/java/frc/robot/subsystems/Autonomous.java
+++ src/main/java/frc/robot/subsystems/Autonomous.java
@@ -22,7 +22,7 @@ public class Autonomous extends Subsystem{
         if(timer.get() <= 13 && timer.get() > 0){
             Robot.m_HopperInatkeSub.moveToS(0.5, 0.5);
             Robot.m_sShooterSub.shoot(0.5);
-        }if(timer.get() > 13 && timer.get() < 15){
+        }else if(timer.get() > 13 && timer.get() < 15){
             Robot.m_ChassisSubsystem.moveForward(1, 0.1);
         }else{
             Robot.m_HopperInatkeSub.moveToS(0, 0);
